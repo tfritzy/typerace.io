7197c9b5cfaaac62c0aa2951311448a6f2e0d29c
Tyler <tfritzy@gmail.com>
2026-07-17 21:26:00 -0500

    Playing around with it

--- tui/src/view/wpmChartInterior.ts
+++ tui/src/view/wpmChartInterior.ts
@@ -7,39 +7,24 @@ import {
 import { Game, PlayerProgress } from "../stdb";
 import { THEME } from "../theme";
 import { decodeCharacterHistory, getWpmByBucket } from "../util/wpmCalculator";
+import { braille } from "../util/braille";
 
-const dots = {
-  "00": "⠀",
-
-  "01": "⢀",
-  "02": "⢠",
-  "03": "⢰",
-  "04": "⢸",
-
-  "10": "⡀",
-  "11": "⣀",
-  "12": "⣠",
-  "13": "⣰",
-  "14": "⣸",
-
-  "20": "⡄",
-  "21": "⣄",
-  "22": "⣤",
-  "23": "⣴",
-  "24": "⣼",
-
-  "30": "⡆",
-  "31": "⣆",
-  "32": "⣦",
-  "33": "⣶",
-  "34": "⣾",
-
-  "40": "⡇",
-  "41": "⣇",
-  "42": "⣧",
-  "43": "⣷",
-  "44": "⣿",
-} as const;
+function mapGap(gap: number) {
+  switch (gap) {
+    case 5:
+      return 0;
+    case 4:
+      return 0;
+    case 3:
+      return 4;
+    case 2:
+      return 2;
+    case 1:
+      return 1;
+    default:
+      return 0;
+  }
+}
 
 export class WpmChartInterior extends BoxRenderable {
   private raceStartTime_s: number = 0;
@@ -80,7 +65,7 @@ export class WpmChartInterior extends BoxRenderable {
 
     const bars = new Array<number>(width * 2);
     for (let x = 0; x < width * 2; x++) {
-      bars[x] = Math.floor((wpmByBucket[x] / maxWpm) * height * 4);
+      bars[x] = Math.round((wpmByBucket[x] / maxWpm) * height * 4);
     }
 
     console.log(bars);
@@ -90,13 +75,15 @@ export class WpmChartInterior extends BoxRenderable {
       const secondBar = bars[(x - this.x) * 2 + 1];
       for (let y = this.y; y < this.y + height; y++) {
         const h = height - 1 - (y - this.y);
-        const firstGap = Math.max(Math.min(firstBar - h * 4, 4), 0);
-        const secondGap = Math.max(Math.min(secondBar - h * 4, 4), 0);
-        const key = firstGap.toString() + secondGap.toString();
+        const firstBin = mapGap(firstBar - h * 4);
+        const secondBin = mapGap(secondBar - h * 4);
+
+        const index = (firstBin << 4) | secondBin;
+
         buffer.setCell(
           x,
           y,
-          dots[key as keyof typeof dots] || " ",
+          braille[index],
           RGBA.fromHex(THEME.accent),
           RGBA.fromHex(THEME.bg0_h),
         );
